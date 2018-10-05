02d58cf3c301def62cdb8b01de97eb0889d551f2
kdunee <k.dunikowski@pixel.com.pl>
2018-10-05 14:29:49 +0200

    Moved the files to appropriate locations

--- {{cookiecutter.project_name}}/setup.py
+++ {{cookiecutter.project_name}}/setup.py
@@ -4,11 +4,11 @@ from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 sources = ['src/function.cpp', 'src/function_cuda.cu']
 
 setup(
-    name='{{project_name}}',
+    name='{{cookiecutter.project_name}}',
     version='0.1',
     ext_modules=[
         CUDAExtension(
-            name='{{project_name}}',
+            name='{{cookiecutter.project_name}}',
             sources=sources),
     ],
     cmdclass={
